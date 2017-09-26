f9e4cbfbb283be34778ef789e1ea8bb9d8b217ad
pratas <pratas@ua.pt>
2017-09-26 14:50:14 +0100

    defs

---
+++ src/defs.h
@@ -0,0 +1,50 @@
+#ifndef DEFS_H_INCLUDED
+#define DEFS_H_INCLUDED
+
+#include <stdint.h>
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
+#if defined(_MSC_VER)
+  #include <io.h>
+#else
+  #include <unistd.h>
+#endif
+
+typedef uint64_t ULL;
+typedef uint64_t U64;
+typedef uint32_t U32;
+typedef uint16_t U16;
+typedef uint8_t  U8;
+typedef int64_t  I64;
+typedef int32_t  I32;
+typedef int16_t  I16;
+typedef int8_t   I8;
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+#define RELEASE                1
+#define VERSION                1
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+#define BUFFER_SIZE            65535
+
+#define DEF_HELP               0
+#define DEF_VERSION            0
+#define DEF_VERBOSE            0
+#define DEF_FORCE              0
+#define DEF_THREADS            4
+#define DEF_ALPHA_SIZE         2
+#define DEF_MAXT               10000
+
+#define MIN_THREADS            1
+#define MAX_THREADS            65535
+#define MIN_ALPHA_SIZE         2
+#define MAX_ALPHA_SIZE         255
+#define MIN_MAXT               1
+#define MAX_MAXT               99999999
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+#endif
+
